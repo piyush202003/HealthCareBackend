5494e6cce677fd6725985ff6c2deaf909cb7ff15
pokemon <pokemon30blade@gmail.com>
2026-06-26 22:15:55 +0530

    26_6_commit3

--- patients/views.py
+++ patients/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from rest_framework import generics, status
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
-from yaml import serialize
 
 from .models import Patient
 from .serializers import PatientSerializer
